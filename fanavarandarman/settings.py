f33a8b0f8ac5bb0ab23377fba6f8e070693a7c59
Ali Ghanbari <alighanbari113@gmail.com>
2025-07-18 10:26:50 +0330

    chore settings.py

--- fanavarandarman/settings.py
+++ fanavarandarman/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3)=*2#1sv@$dm6yz6kq4o@2ej5lpqal3-7w6zws1ps(k$&)h8'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
